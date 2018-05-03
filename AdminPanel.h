673c53b0fe589cb70610d38be443abe04aa770df
GlowK <glow.k@poczta.fm>
2018-05-03 17:26:05 +0200

    Admin Panels mockup added

---
+++ AdminPanel.h
@@ -0,0 +1,22 @@
+#ifndef ADMINPANEL_H
+#define ADMINPANEL_H
+
+#include <QDialog>
+
+namespace Ui {
+class AdminPanel;
+}
+
+class AdminPanel : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AdminPanel(QWidget *parent = 0);
+    ~AdminPanel();
+
+private:
+    Ui::AdminPanel *ui;
+};
+
+#endif // ADMINPANEL_H
